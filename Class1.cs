25b546625a26b2f37a8a7f62d6cf24fa5f381575
samic++ <samichentouf+git2@gmail.com>
2025-10-12 12:22:30 +0200

    add example code

--- Class1.cs
+++ Class1.cs
@@ -2,5 +2,11 @@
 
 public class Class1
 {
-
+    private SemaphoreSlim readLock = new(1, 1);
+    public async Task read()
+    {
+        await readLock.WaitAsync();
+        Console.WriteLine("lock succesful");
+        readLock.Release();
+    }
 }
