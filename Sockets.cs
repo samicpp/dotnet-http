8bf4a2077c59080f2f5b65b1a6d03504649c6b1c
SamiC++ <samichentouf+git2@gmail.com>
2025-12-31 00:55:19 +0100

    add some classes for using Socket/Stream directly

--- Sockets.cs
+++ Sockets.cs
@@ -1,5 +1,23 @@
 namespace Samicpp.Http;
 
-// using System.Net.Sockets;
+using System.Net.Security;
+using System.Net.Sockets;
 
 // maybe quic socket in here in the future
+
+public class TcpSocket(NetworkStream stream) : ADualSocket
+{
+    public TcpSocket(Socket socket): this(new NetworkStream(socket, true)) {}
+    override protected NetworkStream Stream { get; } = stream;
+    override public bool IsSecure { get; } = false;
+}
+public class TlsSocket(SslStream stream) : ADualSocket
+{
+    override protected SslStream Stream { get; } = stream;
+    override public bool IsSecure { get; } = true;
+}
+public class UnkownSocket(Stream stream) : ADualSocket
+{
+    override protected Stream Stream { get; } = stream;
+    override public bool IsSecure { get; } = false; // unknown
+}
